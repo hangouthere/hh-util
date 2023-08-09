7793d323f6d503742949259620db1d6c70385f9a
CodexHere <codexhere@outlook.com>
2023-08-09 17:12:13 -0500

    Moved/updated some types to common core utility namespace, and included types def in package.json

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,20 @@
 export * from './Logger.js';
 
+export type JsonStringify<T> = T extends Date
+  ? string
+  : T extends object
+  ? {
+      [k in keyof T]: JsonStringify<T[k]>;
+    }
+  : T;
+
+export type JSONTypes = string | number | boolean | JSONObject | JSONArray | null;
+
+export type JSONObject = {
+  [key: string]: JSONTypes;
+};
+
+export type JSONArray = Array<JSONTypes>;
+
 export const definedProps = <T>(obj: object): T =>
   Object.fromEntries(Object.entries(obj).filter(([_k, v]) => v !== undefined)) as T;
